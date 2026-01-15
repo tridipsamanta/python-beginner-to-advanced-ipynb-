1b512002e653a9a735fe6ef1550cad6233db258e
Tridip Samanta <samantatridip20@gmail.com>
2026-01-16 00:27:59 +0530

    ...Update...

--- practice.py
+++ practice.py
@@ -1,1 +1,15 @@
-# practice py
+n = int(input("Enter array length : "))
+arr = []
+for i in range(n):
+    print(f"Enter {i+1} element : ")
+    arr.append(int(input()))
+key = int(input("Enter key element : "))
+
+def linear_recursive(arr,key,index):
+     if index == len(arr):
+          return -1
+     if arr[index] == key:
+          return index
+     return linear_recursive(arr,key,index+1)
+print("found at : ",linear_recursive(arr,key,0))
+     
