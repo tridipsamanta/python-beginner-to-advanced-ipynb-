8a69cef796f2294bf9cad3ebf8ba56f4e2dab22b
Tridip Samanta <samantatridip20@gmail.com>
2026-01-15 20:56:00 +0530

    sorting...methods

---
+++ bubble_sort.py
@@ -0,0 +1,22 @@
+def insertion_sort_recursive(arr,n):
+    if n<=1:
+        return
+    
+    insertion_sort_recursive(arr,n-1)
+
+    key = arr[n-1]
+    j = n-2
+
+    while j>= 0 and arr[j] > key:
+        arr[j+1] = arr[j]
+    arr[j+1] = key
+
+
+n = int(input("Enter array length : "))
+arr = []
+print("Enter elements : ")
+for i in range(n):
+    arr.append(int(input()))
+
+insertion_sort_recursive(arr,n)
+print(arr)
