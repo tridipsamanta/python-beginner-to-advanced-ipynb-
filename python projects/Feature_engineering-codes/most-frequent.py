8374e471ba940dbcd55b3ee67c0869a3886b4f95
Tridip Samanta <samantatridip20@gmail.com>
2026-01-21 20:19:21 +0530

    ...add feature engineering codes...

---
+++ python projects/Feature_engineering-codes/most-frequent.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from sklearn.impute import SimpleImputer
+
+data = {
+    'Age':[20,30,40,50,np.nan],
+    'Score':[90,40,60,np.nan,78],
+    'Grade':['A','C','B','C','A']
+}
+
+df = pd.DataFrame(data)
+print("Original dataframe : ")
+print(df)
+
+imputer = SimpleImputer(strategy='most_frequent')
+df_imputed = pd.DataFrame(imputer.fit_transform(df[['Age','Score']]),columns=['Age','Score'])
+df['Age'] = df_imputed['Age']
+df['Score'] = df_imputed['Score']
+
+print("After mean calculation : ")
+print(df)
