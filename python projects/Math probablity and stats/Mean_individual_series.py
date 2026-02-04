ab7d717f984b32c243bb174258f51cdb65b49171
Tridip Samanta <samantatridip20@gmail.com>
2026-02-04 23:33:23 +0530

    Delete Mean_individual_series.py

--- python projects/Math probablity and stats/Mean_individual_series.py
+++
@@ -1,8 +0,0 @@
-n = int(input('Enter number of Data values : '))
-arr = []
-for i in range(n):
-    arr.append(int(input('Enter values : ')))
-sum = 0
-for j in arr:
-    sum += j
-print('Mean = ',sum/n)
