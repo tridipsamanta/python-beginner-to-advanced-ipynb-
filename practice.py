0059249644b8b60f2195b1529dffe4718770a8f5
Tridip Samanta <samantatridip20@gmail.com>
2026-02-11 19:43:38 +0530

    ....

--- practice.py
+++ practice.py
@@ -1,4 +1,27 @@
-import module
+#positional arguments
+def add(a,b):
+    print(a+b)
 
-x = 0
-print(module.factorial(x))
+add(10,2)
+
+#keword arguments
+def student(name,roll):
+    print(name,roll)
+student(roll=2616,name='Tridip')
+# default arguments
+def show(name='User'):
+    print('Hello',name)
+show()
+show('Tridip')
+#variable length arguments
+def total(*numbers):
+    sum = 0
+    for i in numbers:
+        sum += i
+    print(sum)
+total(10,20,30,40)
+#keyword variable length arguments
+def details(**info):
+    for key,value in info.items():
+        print(key,value)
+details(name='Tridip',roll=2616,dept='BCA')
